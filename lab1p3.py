737273b968cdbd20abb55a7ab999a1e265b122b5
Hippoglyph <jonathanrinnarv@hotmail.com>
2018-11-21 14:24:16 +0100

    Q visualized

--- lab1p3.py
+++ lab1p3.py
@@ -25,7 +25,7 @@ class State:
 			return True
 		return False
 
-	def initState(self):
+	def initState():
 		return State(rS[0], rS[1], pS[0], pS[1])
 
 	def isSameState(self, state):
@@ -52,13 +52,12 @@ def canPoliceMove(state, action):
 		return False
 	return True
 
-def QLearn(iterations):
+def QLearn(iterations, res):
 	Q = np.zeros((maxR+1, maxC+1, maxR+1, maxC+1, len(actions)))
 	alpha = np.zeros((maxR+1, maxC+1, maxR+1, maxC+1, len(actions)))
 	lam = 0.8
 	state = State(rS[0],rS[1],pS[0],pS[1])
-	everyN = 1000
-	values = np.zeros((iterations//everyN))
+	values = np.zeros((iterations//res))
 	counter = 0
 
 
@@ -71,17 +70,25 @@ def QLearn(iterations):
 		step = 1/(alpha[state.rR, state.rC, state.pR, state.pC, ai]**(2.0/3.0))
 		Q[state.rR, state.rC, state.pR, state.pC, ai] += step*(r + lam*max([Q[nextState.rR, nextState.rC, nextState.pR, nextState.pC, i] for i in range(len(actions))]) - Q[state.rR, state.rC, state.pR, state.pC, ai])
 		state = nextState
-		if t % everyN == 0:
+		if t % res == 0:
 			values[counter] = Q[rS[0], rS[1], pS[0], pS[1], 2]
 			counter+=1
-	plt.plot(values)
-	plt.show()
+
+	pi = np.zeros((maxR+1, maxC+1, maxR+1, maxC+1), dtype=int)
+	for rR in range(maxR + 1):
+		for rC in range(maxC + 1):
+			for pR in range(maxR + 1):
+				for pC in range(maxC + 1):
+					pi[rR, rC, pR, pC] = np.argmax([Q[rR, rC, pR, pC, i] for i in range(len(actions))])
+
+	return pi, values
+
 
 
 def getNextState(state, action):
 	newState = State(state.rR, state.rC, state.pR, state.pC)
 	if state.isTaken():
-		return state.initState()
+		return State.initState()
 
 	if action == "L":
 		newState.rC -= 1
@@ -146,14 +153,7 @@ def policeWalk(state):
 			return actions[i+1]
 	return actions[-1]
 
-def copy(take, give):
-	for rR in range(maxR + 1):
-		for rC in range(maxC + 1):
-			for pR in range(maxR + 1):
-				for pC in range(maxC + 1):
-					take[rR,rC,pR,pC] = give[rR,rC,pR,pC]
-
-def create_grid(pi, valState, rR, rC, pR, pC):
+def create_grid(pi, rR, rC, pR, pC):
     w = c.winfo_width() # Get current width of canvas
     h = c.winfo_height() # Get current height of canvas
     c.delete('grid_line') # Will only remove the grid_line
@@ -173,71 +173,18 @@ def create_grid(pi, valState, rR, rC, pR, pC):
     		if (row == bank[0] and col == bank[1]):
     			c.create_text((col)*colWidth+colWidth*0.85, (row)*rowWidth + rowWidth*0.75, text=u'\u2721', font=("Helvetica", colWidth//6), tag='grid_line')
     		c.create_text((col)*colWidth+colWidth*0.5, (row)*rowWidth + rowWidth*0.33, text=actions[pi[row,col, pR, pC]], font=("Helvetica", colWidth//5), tag='grid_line')
-    		c.create_text((col)*colWidth+colWidth*0.5, (row)*rowWidth + rowWidth*0.66, text="{:.2f}".format(valState[row, col, pR, pC]), font=("Helvetica", colWidth//6), tag='grid_line')
+    		#c.create_text((col)*colWidth+colWidth*0.5, (row)*rowWidth + rowWidth*0.66, text="{:.2f}".format(valState[row, col, pR, pC]), font=("Helvetica", colWidth//6), tag='grid_line')
 
     c.create_text((pC)*colWidth+colWidth*0.75, (pR)*rowWidth + rowWidth*0.33, text=u'\u2620', font=("Helvetica", colWidth//4), tag='grid_line')
     c.create_text((rC)*colWidth+colWidth*0.25, (rR)*rowWidth + rowWidth*0.33, text=u'\u263a', font=("Helvetica", colWidth//4), tag='grid_line')
 
-def policeMoveRollOut(rR, rC, pR, pC):
-	state = State(rR, rC, pR, pC)
-
-	if state.isTaken():
-		return (pS[0], pS[1])
-
-	goalStateUp = State(rR, rC, max(pR-1, 0), pC)
-	goalStateDown = State(rR, rC, min(pR+1, maxR), pC)
-	goalStateLeft = State(rR, rC, pR , max(pC-1, 0))
-	goalStateRight = State(rR, rC, pR , min(pC+1, maxC))
-
-	trans = [policeTransitions(goalStateUp, state, "U"), policeTransitions(goalStateDown, state, "D"), policeTransitions(goalStateLeft, state, "L"), policeTransitions(goalStateRight, state, "R")]
-
-	cumSum = np.cumsum(trans)
-
-	val = random.random()
-
-	for i in range(len(cumSum)):
-		if cumSum[i] > val:
-			if i == 0:
-				return (pR - 1, pC)
-			if i == 1:
-				return (pR + 1, pC)
-			if i == 2:
-				return (pR, pC - 1)
-			if i == 3:
-				return (pR, pC + 1)
-	return (pR, pC)
-
-def robberMoveRollOut(pi, rR, rC, pR, pC):
-	state = State(rR, rC, pR, pC)
-	if state.isTaken():
-		return (rS[0], rS[1])
-
-	a = actions[pi[rR, rC, pR, pC]]
-	if a == "L":
-		rC -= 1
-		rC = max(rC, 0)
-	elif a == "R":
-		rC += 1
-		rC = min(maxC, rC)
-	elif a == "U":
-		rR -= 1
-		rR = max(rR, 0)
-	elif a == "D":
-		rR += 1
-		rR = min(maxR, rR)
-	return (rR, rC)
-
-def advanceGame(pi):
-	global gameRR
-	global gameRC
-	global gamePR
-	global gamePC
-
-	newRR, newRC = robberMoveRollOut(pi, gameRR, gameRC, gamePR, gamePC)
-	gamePR, gamePC = policeMoveRollOut(gameRR, gameRC, gamePR, gamePC)
-	gameRR = newRR	
-	gameRC = newRC
-	create_grid(pi, valState, gameRR, gameRC, gamePR, gamePC)
+def advanceGame(pi, state):
+	nextState = getNextState(state, actions[pi[state.rR, state.rC, state.pR, state.pC]])
+	state.rR = nextState.rR
+	state.rC = nextState.rC
+	state.pR = nextState.pR
+	state.pC = nextState.pC
+	create_grid(pi, state.rR, state.rC, state.pR, state.pC)
 
 bank = (1,1)
 
@@ -250,12 +197,9 @@ rS = (0,0)
 maxR = 3
 maxC = 3
 
-QLearn(100000)
+pi, qValues = QLearn(100000, 1000)
 
-gameRR = rS[0]
-gameRC = rS[1]
-gamePR = pS[0]
-gamePC = pS[1]
+stateRollOut = State.initState()
 
 root = tk.Tk()
 
@@ -263,8 +207,8 @@ c = tk.Canvas(root, height=250, width=500, bg='white')
 c.pack(fill=tk.BOTH, expand=True)
 
 
-#c.bind('<Configure>', lambda e: create_grid(pi, valState, rS[0], rS[1], pS[0], pS[1]))
-#c.bind_all('<Return>', lambda e: advanceGame(pi))
+c.bind('<Configure>', lambda e: create_grid(pi, rS[0], rS[1], pS[0], pS[1]))
+c.bind_all('<Return>', lambda e: advanceGame(pi, stateRollOut))
 
 #c.bind_all('<Left>', lambda e,val = -1: changeDebug(val))
 #c.bind_all('<Right>', lambda e,val = 1: changeDebug(val))
